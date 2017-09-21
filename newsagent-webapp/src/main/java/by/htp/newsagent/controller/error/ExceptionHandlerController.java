818fcfee36255e27447a47e3c697dc505066ecd7
Dzianis Vasileuski <d.vasileuski@gmail.com>
2017-09-21 20:28:17 +0300

    404 handling were added

--- newsagent-webapp/src/main/java/by/htp/newsagent/controller/error/ExceptionHandlerController.java
+++ newsagent-webapp/src/main/java/by/htp/newsagent/controller/error/ExceptionHandlerController.java
@@ -9,49 +9,66 @@ import org.springframework.context.MessageSource;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.http.HttpStatus;
 
 import by.htp.newsagent.model.Location;
 import by.htp.newsagent.model.LocationWebModel;
 import by.htp.newsagent.service.exception.NewsValidationException;
 
-
-
 @ControllerAdvice
 public class ExceptionHandlerController {
 	private static final Logger LOGGER = LogManager.getLogger(ExceptionHandlerController.class);
-	
+
 	@Autowired
 	private MessageSource messageSource;
 
 	@ExceptionHandler
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public String handleGlobalExceptions(Exception e, Model model, Locale locale) {
 		LocationWebModel locationModel = new LocationWebModel();
-		
+
 		LOGGER.error("Exception occurred, ", e);
 		e.printStackTrace();
-		
+
 		locationModel.setCurrentLocation(Location.ERROR);
 		locationModel.setPreviousLocation(Location.NEWS);
-		
+
 		model.addAttribute("locationModel", locationModel);
 		model.addAttribute("errorMessage", messageSource.getMessage("message.internalError", null, locale));
 
 		return "error";
 	}
-	
+
 	@ExceptionHandler
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public String handleValidationExceptions(NewsValidationException e, Model model, Locale locale) {
 		LocationWebModel locationModel = new LocationWebModel();
-		
+
 		LOGGER.error("NewsValidation Exception occurred, ", e);
 		e.printStackTrace();
-		
+
 		locationModel.setCurrentLocation(Location.ERROR);
 		locationModel.setPreviousLocation(Location.NEWS);
-		
+
 		model.addAttribute("locationModel", locationModel);
 		model.addAttribute("errorMessage", messageSource.getMessage("message.illegalNewsArgument", null, locale));
-		
+
+		return "error";
+	}
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String handleResourceNotFoundException(Model model, Locale locale) {
+		LocationWebModel locationModel = new LocationWebModel();
+
+		locationModel.setCurrentLocation(Location.ERROR);
+		locationModel.setPreviousLocation(Location.NEWS);
+
+		model.addAttribute("locationModel", locationModel);
+		model.addAttribute("errorMessage", messageSource.getMessage("message.NotFound404", null, locale));
+
 		return "error";
 	}
 }
